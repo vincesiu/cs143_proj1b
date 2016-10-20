1318a8140889f134167c3e63014b87dca9f3e2e1
Vincent Siu <vince.siu4@gmail.com>
2016-10-19 22:05:11 -0700

    Update reset script so it drops Director

--- reset.sh
+++ reset.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-echo "DROP TABLE IF EXISTS Movie, Actor, MovieDirector, MovieGenre, Review, MovieActor, MaxPersonID, MaxMovieID" | mysql TEST
+echo "DROP TABLE IF EXISTS Movie, Actor, Director, MovieDirector, MovieGenre, Review, MovieActor, MaxPersonID, MaxMovieID" | mysql TEST
+
+mysql TEST < create.sql
+mysql TEST < load.sql
