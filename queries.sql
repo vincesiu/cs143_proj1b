c24a9e5a7fc554a40debcb13203ad5f7b199bae9
Vincent Siu <vince.siu4@gmail.com>
2016-10-19 21:53:31 -0700

    Add the three queries

--- queries.sql
+++ queries.sql
@@ -1,3 +1,5 @@
+-- Query 1
+-- This query selects all actors who were in the movie 'Die Another Day'
 SELECT first,last 
 FROM Actor INNER JOIN (
 	SELECT aid FROM MovieActor 
@@ -6,8 +8,19 @@ FROM Actor INNER JOIN (
 	WHERE Movie.title = 'Die Another Day') AS Actors 
 ON Actor.id = Actors.aid;
 
+-- Query 2
+-- This query gets the number of actors who acted in multiple movies
 SELECT count(*) FROM (
 	SELECT aid
 	FROM MovieActor
 	GROUP BY aid
 	HAVING count(mid) > 1) AS ActorsInMultipleMovies;
+  
+-- Query 3
+-- This query gets ids of actors who acted in multiple genres
+SELECT MovieActor.aid
+FROM MovieGenre
+JOIN MovieActor
+ON MovieActor.mid = MovieGenre.mid
+GROUP BY MovieActor.aid
+HAVING count(DISTINCT MovieGenre.genre > 1)
